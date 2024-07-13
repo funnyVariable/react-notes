13f7b92da660b22f52b9ea2345ec77ef06a5e923
uhmdkhms <uhmdkhms@gmail.com>
2024-07-13 18:12:38 +0300

    initial layout, Normalize.css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import Notes from "./Notes";
+import "./Normalize.css";
+import "./App.css";
+import "./Notes.css";
+import Editor from "./Editor";
+
 function App() {
-  return <div className="App">App.js</div>;
+  return (
+    <div className="app">
+      <Notes />
+      <div className="slider"></div>
+      <Editor />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Notes.js
@@ -0,0 +1,10 @@
+export default function Notes() {
+  return (
+    <div className="notes">
+      <div>Note1</div>
+      <div>Note2</div>
+      <div>Note3</div>
+      <div>Note4</div>
+    </div>
+  );
+}
