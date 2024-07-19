616766ff664b7b70c0d4c0acc7cb1483f7ba3159
uhmdkhms <uhmdkhms@gmail.com>
2024-07-19 10:06:42 +0300

    basic scroll area logic

--- src/Editor.js
+++ src/Editor.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import xmark from "./xmark.svg";
 
 export default function Editor() {
@@ -7,26 +7,75 @@ export default function Editor() {
 
   const scrollAreaLeft = useRef(null);
   const scrollAreaRight = useRef(null);
+  const fastScrollAreaLeft = useRef(null);
+  const fastScrollAreaRight = useRef(null);
+
+  let scrollInterval;
+
+  function scroll(direction, fast) {
+    scrollInterval = setInterval(() => {
+      if (direction === "left") {
+        tabs.current.scrollLeft -= fast ? 5 : 1;
+      }
+      if (direction === "right") {
+        tabs.current.scrollLeft += fast ? 5 : 1;
+      }
+    }, 25);
+  }
+  function stopScrolling(interval) {
+    clearInterval(interval);
+  }
 
   useEffect(() => {
     let totalTabsWidth = 0;
-    for (let i = 2; i < tabs.current.children.length; i++) {
+    for (let i = 4; i < tabs.current.children.length; i++) {
       totalTabsWidth += tabs.current.children[i].offsetWidth;
     }
 
     if (totalTabsWidth > tabs.current.offsetWidth) {
       scrollAreaLeft.current.classList.add("active-scroll-area");
       scrollAreaRight.current.classList.add("active-scroll-area");
+
+      fastScrollAreaLeft.current.classList.add("active-scroll-area", "fast");
+      fastScrollAreaRight.current.classList.add("active-scroll-area", "fast");
+
+      scrollAreaLeft.current.style.left = `${tabs.current.offsetLeft + 40}px`;
+      fastScrollAreaLeft.current.style.left = `${tabs.current.offsetLeft}px`;
     } else {
       scrollAreaLeft.current.classList.remove("active-scroll-area");
       scrollAreaRight.current.classList.remove("active-scroll-area");
+
+      fastScrollAreaLeft.current.classList.remove("active-scroll-area", "fast");
+      fastScrollAreaRight.current.classList.remove(
+        "active-scroll-area",
+        "fast"
+      );
     }
   });
+
   return (
     <div className="editor" ref={editor}>
       <div className="tabs" ref={tabs}>
-        <span ref={scrollAreaLeft}></span>
-        <span ref={scrollAreaRight}></span>
+        <span
+          ref={scrollAreaLeft}
+          onMouseEnter={() => scroll("left")}
+          onMouseLeave={() => stopScrolling(scrollInterval)}
+        ></span>
+        <span
+          ref={fastScrollAreaLeft}
+          onMouseEnter={() => scroll("left", true)}
+          onMouseLeave={() => stopScrolling(scrollInterval)}
+        ></span>
+        <span
+          ref={scrollAreaRight}
+          onMouseEnter={() => scroll("right")}
+          onMouseLeave={() => stopScrolling(scrollInterval)}
+        ></span>
+        <span
+          ref={fastScrollAreaRight}
+          onMouseEnter={() => scroll("right", true)}
+          onMouseLeave={() => stopScrolling(scrollInterval)}
+        ></span>
         <div>
           tab1
           <img src={xmark} alt="" />
