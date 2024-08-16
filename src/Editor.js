fc5693ee45cd9474bc9af427718dad7a424e660f
uhmdkhms <uhmdkhms@gmail.com>
2024-08-17 00:59:42 +0300

    removed scroll areas

--- src/Editor.js
+++ src/Editor.js
@@ -24,12 +24,6 @@ export default function Editor({ toggle, setToggle }) {
   const currentTab = getTabById(currentTabId);
   const isTabUnsaved = useContext(EditorContext).isTabUnsaved;
 
-  const scrollAreaLeft = useRef(null);
-  const scrollAreaRight = useRef(null);
-  const fastScrollAreaLeft = useRef(null);
-  const fastScrollAreaRight = useRef(null);
-  const scrollLight = useRef(null);
-
   const [noteTitle, setNoteTitle] = useState("");
   const [currentInput, setCurrentInput] = useState(
     currentTab ? currentTab.input : ""
@@ -60,33 +54,6 @@ export default function Editor({ toggle, setToggle }) {
     setCurrentTabId(id);
   }
 
-  let scrollInterval;
-  let tabsOverflow = false;
-  let totalTabsWidth = 0;
-
-  function scroll(direction, fast) {
-    scrollInterval = setInterval(() => {
-      if (direction === "left") {
-        tabBar.current.scrollLeft -= fast ? 5 : 1;
-      }
-      if (direction === "right") {
-        tabBar.current.scrollLeft += fast ? 5 : 1;
-      }
-    }, 25);
-
-    if (tabsOverflow) {
-      tabBar.current.classList.add(
-        direction === "left" ? "scrolling-left" : "scrolling-right"
-      );
-      scrollLight.current.classList.add("scroll-effect");
-    }
-  }
-  function stopScrolling(interval) {
-    clearInterval(interval);
-    tabBar.current.classList.remove("scrolling-left", "scrolling-right");
-    scrollLight.current.classList.remove("scroll-effect");
-  }
-
   function newNote() {
     const id = generateNoteId();
     const newNote = {
@@ -103,36 +70,6 @@ export default function Editor({ toggle, setToggle }) {
   }
 
   useEffect(() => {
-    for (let i = 5; i < tabBar.current.children.length; i++) {
-      totalTabsWidth += tabBar.current.children[i].offsetWidth;
-    }
-
-    if (totalTabsWidth > tabBar.current.offsetWidth) {
-      tabsOverflow = true;
-
-      scrollAreaLeft.current.classList.add("active-scroll-area");
-      scrollAreaRight.current.classList.add("active-scroll-area");
-
-      fastScrollAreaLeft.current.classList.add("active-scroll-area", "fast");
-      fastScrollAreaRight.current.classList.add("active-scroll-area", "fast");
-
-      scrollAreaLeft.current.style.left = `${tabBar.current.offsetLeft + 20}px`;
-      fastScrollAreaLeft.current.style.left = `${tabBar.current.offsetLeft}px`;
-    } else {
-      tabsOverflow = false;
-
-      scrollAreaLeft.current.classList.remove("active-scroll-area");
-      scrollAreaRight.current.classList.remove("active-scroll-area");
-
-      fastScrollAreaLeft.current.classList.remove("active-scroll-area", "fast");
-      fastScrollAreaRight.current.classList.remove(
-        "active-scroll-area",
-        "fast"
-      );
-    }
-  }, [tabs]);
-
-  useEffect(() => {
     if (currentNote) {
       currentTab && currentTab.input
         ? setCurrentInput(currentTab.input)
@@ -146,27 +83,6 @@ export default function Editor({ toggle, setToggle }) {
   return (
     <div className="editor" ref={editor}>
       <div className="tab-bar" ref={tabBar}>
-        <i ref={scrollLight}></i>
-        <span
-          ref={scrollAreaLeft}
-          onMouseEnter={() => scroll("left")}
-          onMouseLeave={() => stopScrolling(scrollInterval)}
-        ></span>
-        <span
-          ref={fastScrollAreaLeft}
-          onMouseEnter={() => scroll("left", true)}
-          onMouseLeave={() => stopScrolling(scrollInterval)}
-        ></span>
-        <span
-          ref={scrollAreaRight}
-          onMouseEnter={() => scroll("right")}
-          onMouseLeave={() => stopScrolling(scrollInterval)}
-        ></span>
-        <span
-          ref={fastScrollAreaRight}
-          onMouseEnter={() => scroll("right", true)}
-          onMouseLeave={() => stopScrolling(scrollInterval)}
-        ></span>
         {tabs.map((tab, key) => (
           <div
             key={key}
